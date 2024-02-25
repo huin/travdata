b1c0123e4f79cfee55b3c5dbcfad2bb07ff13127
John Beisley <johnbeisleyuk@gmail.com>
2024-02-25 09:42:22 +0000

    feat: provide ehex parsing/formatting functions.

--- parseutil.py
+++ parseutil.py
@@ -81,3 +81,48 @@ def parse_credits(s: str) -> int:
 def d66_enum() -> Iterator[str]:
     for i in range(36):
         yield f"{1 + i // 6}{1 + i % 6}"
+
+
+_EHEX_TO_INT: dict[str, int] = {}
+_INT_TO_EHEX: list[str] = []
+
+
+def __init_ehex() -> None:
+    global _EHEX_TO_INT, _INT_TO_EHEX
+    ranges: list[tuple[str, str]] = [
+        ("0", "9"),
+        ("A", "H"),
+        ("J", "N"),
+        ("P", "Z"),
+    ]
+    value: int = 0
+    for start, end in ranges:
+        istart = ord(start)
+        iend = ord(end)
+        for chr_ord in range(istart, iend + 1):
+            c = chr(chr_ord)
+            _INT_TO_EHEX.append(c)
+            _EHEX_TO_INT[c] = value
+            value += 1
+
+
+__init_ehex()
+
+
+def parse_ehex_char(c: str) -> int:
+    """Parses a single Ehex digit."""
+    # https://wiki.travellerrpg.com/Hexadecimal_Notation
+    try:
+        return _EHEX_TO_INT[c]
+    except KeyError:
+        raise ValueError(c)
+
+
+def fmt_ehex_char(v: int) -> str:
+    """Formats an integer in the Ehex digit range [0,33] to the Ehex digit."""
+    if v < 0:
+        raise ValueError(v)
+    try:
+        return _INT_TO_EHEX[v]
+    except IndexError:
+        raise ValueError(v)
---
+++ parseutil_test.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+import unittest
+
+import parseutil
+
+
+class ParseEhex(unittest.TestCase):
+
+    def test_parse_valid(self) -> None:
+        cases: list[tuple[str, int]] = [
+            # (input, want)
+            ("0", 0),
+            ("5", 5),
+            ("9", 9),
+            ("A", 10),
+            ("H", 17),
+            ("J", 18),
+            ("N", 22),
+            ("P", 23),
+            ("Z", 33),
+        ]
+        for inp, want in cases:
+            with self.subTest(inp):
+                got = parseutil.parse_ehex_char(inp)
+                self.assertEqual(got, want)
+
+    def test_parse_invalid(self) -> None:
+        cases: list[parseutil.EhexDigit] = [
+            "I",
+            "O",
+            "a",
+            "h",
+            ".",
+        ]
+        for inp in cases:
+            with self.assertRaises(ValueError):
+                parseutil.parse_ehex_char(inp)
+
+    def test_fmt_valid(self) -> None:
+        cases: list[tuple[int, str]] = [
+            # (input, want)
+            (0 , "0"),
+            (5 , "5"),
+            (9 , "9"),
+            (10, "A"),
+            (17, "H"),
+            (18, "J"),
+            (22, "N"),
+            (23, "P"),
+            (33, "Z"),
+        ]
+        for inp, want in cases:
+            with self.subTest(inp):
+                got = parseutil.fmt_ehex_char(inp)
+                self.assertEqual(got, want)
+
+    def test_fmt_invalid(self) -> None:
+        cases: list[int] = [
+            -3,
+            34,
+        ]
+        for inp in cases:
+            with self.assertRaises(ValueError):
+                parseutil.fmt_ehex_char(inp)
+
+
+if __name__ == "__main__":
+    unittest.main()
