cce6b81bb63269fcdead3de6873bf0af002abe46
John Beisley <johnbeisleyuk@gmail.com>
2024-03-03 17:16:45 +0000

    fix: fix YAML type parameter

--- src/travdata/datatypes/yamlcodec.py
+++ src/travdata/datatypes/yamlcodec.py
@@ -7,7 +7,7 @@ from ruamel import yaml
 T = TypeVar("T")
 
 # Provides a YAML instance that will safely read and write datatypes.
-DATATYPES_YAML = yaml.YAML(typ="pure")
+DATATYPES_YAML = yaml.YAML(typ="safe")
 DATATYPES_YAML.indent(mapping=2, sequence=4, offset=2)
 
 
