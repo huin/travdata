97243ab779f97a8459e5e130ab274a86fb2860fd
John Beisley <johnbeisleyuk@gmail.com>
2024-03-24 07:50:34 +0000

    doc: improve licensing build process
    * Prevent license text escaping asciidoc block.
    * Include more path information in the license filename.

--- devscripts/build_licenses.py
+++ devscripts/build_licenses.py
@@ -4,9 +4,11 @@
 
 from importlib import metadata
 import operator
+import os.path
 import pathlib
 import shutil
 import subprocess
+import textwrap
 from typing import cast
 
 import piplicenses_lib
@@ -21,25 +23,9 @@ def main() -> None:
 
     for dep_name in sorted(dep_names):
         dist = metadata.distribution(dep_name)
-        info = piplicenses_lib.get_package_info(dist, include_files=True)
-
-        print(f"== {info['namever']}\n")
-
+        print(f"== {dist.metadata['Name']} {dist.metadata['Version']}\n")
         print_metadata(dist)
-
-        lfiles = cast(list[str], info["licensefile"])
-        ltexts = cast(list[str], info["licensetext"])
-        files: list[tuple[str, str]] = [
-            (pathlib.Path(fname).name, text) for fname, text in zip(lfiles, ltexts)
-        ]
-        files.sort(key=operator.itemgetter(0))
-
-        for fname, text in files:
-            p = pathlib.Path(fname)
-            print(f"=== {p.name}\n")
-            print("[listing]\n----")
-            print(text)
-            print("----\n")
+        print_licenses(dist)
 
 
 def get_dep_names() -> list[str]:
@@ -80,6 +66,35 @@ def print_metadata(dist: metadata.Distribution) -> None:
         displayed_metadata = True
     if displayed_metadata:
         print()
+
+
+def print_licenses(dist: metadata.Distribution) -> None:
+    """Prints each discovered license file and content."""
+    info = piplicenses_lib.get_package_info(dist, include_files=True)
+
+    lfiles = cast(list[str], info["licensefile"])
+    lpaths: list[pathlib.PurePath]
+    if len(lfiles) > 1:
+        comm_path = pathlib.PurePath(os.path.commonpath(lfiles))
+        lpaths = [pathlib.PurePath(p).relative_to(comm_path) for p in lfiles]
+    else:
+        lpaths = [pathlib.PurePath(p) for p in lfiles]
+
+    ltexts = cast(list[str], info["licensetext"])
+    files: list[tuple[pathlib.PurePath, str]] = list(zip(lpaths, ltexts))
+    files.sort(key=operator.itemgetter(0))
+
+    for lpath, text in files:
+        if lpath.is_absolute():
+            fname = lpath.name
+        else:
+            fname = str(lpath)
+        print(f"=== {fname}\n")
+        print("[listing]\n----")
+        text = textwrap.dedent(text)
+        text = textwrap.indent(text, "  ")
+        print(text)
+        print("----\n")
 
 
 if __name__ == "__main__":
