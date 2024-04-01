e4a0ca2cdb06deea85298656ecc37fc5752a9e41
John Beisley <johnbeisleyuk@gmail.com>
2024-04-01 15:32:14 +0100

    doc: include prominent information about data usage in GUI
    Signed-off-by: John Beisley <johnbeisleyuk@gmail.com>

---
+++ src/travdata/commontext.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""Commonly used text throughout the program."""
+
+DATA_USAGE = """\
+The extracted and produced data is *not* for redistribution, as it is almost
+certainly subject to copyright. This utility (and its output) is intended as an
+aid to those who legally own a copy of the Mongoose Traveller materials, and
+wish to make use of the data for their own purposes.
+
+It is the sole responsibility of the user of this program to use the extracted
+data in a manner that respects the publisher's IP rights.
+"""
