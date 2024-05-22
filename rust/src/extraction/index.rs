d633322b11b7acd556de8b22835012de8817b5cc
John Beisley <johnbeisleyuk@gmail.com>
2024-05-22 22:42:26 +0100

    chore(rust-impl): implement index::IndexWriter

--- rust/src/extraction/index.rs
+++ rust/src/extraction/index.rs
@@ -6,17 +6,22 @@ use std::{
     path::{Path, PathBuf},
 };
 
-use anyhow::Result;
+use anyhow::{bail, Result};
 use serde::{Deserialize, Serialize};
 
 use crate::{
-    config::{book::Table, root::Book},
-    filesio::{FileWrite, ReadWriter, Reader},
+    config::{
+        book::{self, Table},
+        root::Book,
+    },
+    filesio::{FileRead, FileWrite, FilesIoError, ReadWriter, Reader},
+    fmtutil,
 };
 
 type CsvResult<T> = std::result::Result<T, csv::Error>;
 
 const INDEX_PATH: &str = "index.csv";
+const ITEMS_DELIM: &str = ";";
 
 /// Index of all extracted tables in an output.
 pub struct Index {
@@ -33,7 +38,7 @@ impl Index {
         for record_result in records {
             let record = record_result?;
 
-            for tag in record.tags.split(";") {
+            for tag in record.tags.split(ITEMS_DELIM) {
                 tags_to_paths
                     .entry(tag.to_owned())
                     .or_default()
@@ -51,8 +56,7 @@ impl Index {
 
     fn load_from_read<R: Read>(r: R) -> Result<Self> {
         let mut reader = csv::Reader::from_reader(r);
-        let records = reader
-            .deserialize::<CsvRecord>();
+        let records = reader.deserialize::<CsvRecord>();
         Index::from_records(records)
     }
 
@@ -100,8 +104,96 @@ impl Index {
         }
     }
 }
-        todo!()
+
+/// Creates or updates an index.
+pub struct IndexWriter<'rw> {
+    write_file: FileWrite<'rw>,
+    entries: HashMap<PathBuf, WriteRecord>,
+}
+
+impl<'rw> IndexWriter<'rw> {
+    pub fn new(read_writer: &dyn ReadWriter<'rw>) -> Result<Self> {
+        let mut entries: HashMap<PathBuf, WriteRecord> = HashMap::new();
+        match read_writer.open_read(Path::new(INDEX_PATH)) {
+            Ok(mut r) => Self::load_entries(&mut r, &mut entries)?,
+            Err(err) if FilesIoError::NotFound.eq_anyhow(&err) => {}
+            Err(err) => bail!(err),
+        }
+
+        let write_file = read_writer.open_write(Path::new(INDEX_PATH))?;
+
+        Ok(Self {
+            write_file,
+            entries,
+        })
     }
+
+    fn load_entries(r: &mut FileRead, entries: &mut HashMap<PathBuf, WriteRecord>) -> Result<()> {
+        let mut reader = csv::Reader::from_reader(r);
+        for record_result in reader.deserialize::<CsvRecord>() {
+            let record = record_result?;
+            entries.insert(
+                record.table_path,
+                WriteRecord {
+                    pages: record.pages,
+                    tags: record.tags,
+                },
+            );
+        }
+        Ok(())
+    }
+
+    /// Commits entries to the index file.
+    pub fn commit(mut self) -> Result<()> {
+        let mut w = csv::Writer::from_writer(&mut self.write_file);
+        for (table_path, write_record) in self.entries {
+            w.serialize(CsvRecord {
+                table_path,
+                pages: write_record.pages,
+                tags: write_record.tags,
+            })?;
+        }
+        w.flush()?;
+        drop(w);
+        self.write_file.commit()
+    }
+
+    /// Write an index entry.
+    ///
+    /// * `output_path` Path to the table file within the output.
+    /// * `table` Table being output.
+    /// * `book_cfg` Book configuration.
+    /// * `pages` Page numbers that the entry was sourced from.
+    pub fn add_entry(
+        &mut self,
+        output_path: PathBuf,
+        table: &Table,
+        book_cfg: &Book,
+        pages: &[i32],
+    ) {
+        let mut sorted_tags: Vec<String> = table.tags.iter().map(String::clone).collect();
+        sorted_tags.sort();
+
+        let mut sorted_pages: Vec<i32> = pages
+            .iter()
+            .map(|page| book_cfg.page_offset + page)
+            .collect();
+        sorted_pages.sort();
+
+        self.entries.insert(
+            output_path,
+            WriteRecord {
+                pages: fmtutil::join_display_slice(pages, ITEMS_DELIM),
+                tags: sorted_tags.join(ITEMS_DELIM),
+            },
+        );
+    }
+}
+
+struct WriteRecord {
+    pages: String,
+    tags: String,
+}
 
 #[derive(Debug, Deserialize, Serialize)]
 struct CsvRecord {
@@ -119,7 +211,13 @@ mod tests {
         matchers::{eq, unordered_elements_are},
     };
 
-    use crate::{extraction::index::INDEX_PATH, filesio::{mem::{MemFilesHandle, MemReadWriter}, ReadWriter}};
+    use crate::{
+        extraction::index::INDEX_PATH,
+        filesio::{
+            mem::{MemFilesHandle, MemReadWriter},
+            ReadWriter,
+        },
+    };
 
     use super::Index;
 
@@ -127,12 +225,17 @@ mod tests {
     fn test_index_paths_with_all_tags() {
         let read_writer = MemReadWriter::new(MemFilesHandle::default());
 
-        let mut w = read_writer.open_write(Path::new(INDEX_PATH)).expect("should open");
-        w.write_all(b"table_path,pages,tags
+        let mut w = read_writer
+            .open_write(Path::new(INDEX_PATH))
+            .expect("should open");
+        w.write_all(
+            b"table_path,pages,tags
 file-a,,tag-a;tag-d;tag-z
 file-b,,tag-b;tag-d;tag-z
 file-c,,tag-c;tag-z
-").expect("should write");
+",
+        )
+        .expect("should write");
         w.commit().expect("should commit");
 
         let index = Index::load(&read_writer).expect("should create from reader");
@@ -153,5 +256,10 @@ file-c,,tag-c;tag-z
             index.paths_with_all_tags(&vec!["tag-d", "tag-z"]),
             unordered_elements_are![eq(Path::new("file-a")), eq(Path::new("file-b"))],
         );
+    }
+
+    #[test]
+    fn test_writes_new_index() {
+        // TODO
     }
 }
