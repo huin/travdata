6c6795bd321f12a78feb02915de20f6227a474f2
John Beisley <johnbeisleyuk@gmail.com>
2024-05-23 21:28:31 +0100

    feat(rust-impl): write index

--- rust/src/extraction/bookextract.rs
+++ rust/src/extraction/bookextract.rs
@@ -8,7 +8,7 @@ use crate::{
     filesio::{ReadWriter, Reader},
 };
 
-use super::tableextract::extract_table;
+use super::{index::IndexWriter, tableextract::extract_table};
 
 pub fn extract_book(
     tabula_client: &tabulautil::TabulaClient,
@@ -18,30 +18,66 @@ pub fn extract_book(
     book_name: &str,
     input_pdf: &Path,
 ) -> Result<()> {
-    let top_group = cfg
+    let book_cfg = cfg
         .books
         .get(book_name)
-        .ok_or_else(|| anyhow!("book {:?} does not exist in the configuration", book_name))?
-        .load_group(cfg_reader)?;
+        .ok_or_else(|| anyhow!("book {:?} does not exist in the configuration", book_name))?;
 
-    process_group(tabula_client, cfg_reader, out_writer, &top_group, input_pdf)
+    let top_group = book_cfg.load_group(cfg_reader)?;
+
+    let mut index_writer =
+        IndexWriter::new(out_writer).with_context(|| "opening index for update")?;
+
+    process_group(
+        tabula_client,
+        cfg_reader,
+        out_writer,
+        &mut index_writer,
+        book_cfg,
+        &top_group,
+        input_pdf,
+    )?;
+
+    index_writer
+        .commit()
+        .with_context(|| "commiting changes to the index")?;
+
+    Ok(())
 }
 
 fn process_group(
     tabula_client: &tabulautil::TabulaClient,
     cfg_reader: &dyn Reader,
     out_writer: &dyn ReadWriter,
+    index_writer: &mut IndexWriter,
+    book_cfg: &config::root::Book,
     grp: &config::book::Group,
     input_pdf: &Path,
 ) -> Result<()> {
     for (table_name, table_cfg) in &grp.tables {
-        extract_table(tabula_client, cfg_reader, out_writer, table_cfg, input_pdf)
-            .with_context(|| format!("processing table {:?}", table_name))?;
+        extract_table(
+            tabula_client,
+            cfg_reader,
+            out_writer,
+            index_writer,
+            book_cfg,
+            table_cfg,
+            input_pdf,
+        )
+        .with_context(|| format!("processing table {:?}", table_name))?;
     }
 
     for (child_grp_name, child_grp) in &grp.groups {
-        process_group(tabula_client, cfg_reader, out_writer, child_grp, input_pdf)
-            .with_context(|| format!("processing group {:?}", child_grp_name))?;
+        process_group(
+            tabula_client,
+            cfg_reader,
+            out_writer,
+            index_writer,
+            book_cfg,
+            child_grp,
+            input_pdf,
+        )
+        .with_context(|| format!("processing group {:?}", child_grp_name))?;
     }
 
     Ok(())
