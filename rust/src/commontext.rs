8b767deacdb7a02d8e1c66f76f27a181fe97d5e8
John Beisley <johnbeisleyuk@gmail.com>
2024-06-08 14:04:11 +0100

    feat: first mockup of extraction GUI

---
+++ rust/src/commontext.rs
@@ -0,0 +1,9 @@
+//! Commonly used text throughout the program.
+
+pub const DATA_USAGE: &str = r#"The extracted and produced data is *not* for redistribution, as it is almost
+certainly subject to copyright. This utility (and its output) is intended as an
+aid to those who legally own a copy of the Mongoose Traveller materials, and
+wish to make use of the data for their own purposes.
+
+It is the sole responsibility of the user of this program to use the extracted
+data in a manner that respects the publisher's IP rights."#;
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,10 +1,12 @@
 use anyhow::Result;
 
 mod cli;
+mod commontext;
 mod config;
 mod extraction;
 mod filesio;
 mod fmtutil;
+mod gui;
 mod table;
 #[cfg(test)]
 mod testutil;
