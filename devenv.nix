31042ede49e99caaef3744d7a737583cdfe521f3
John Beisley <johnbeisleyuk@gmail.com>
2024-06-29 20:55:37 +0100

    chore: setup devenv

---
+++ devenv.nix
@@ -0,0 +1,29 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  # https://devenv.sh/basics/
+  env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  packages = with pkgs; [
+    git
+    poetry
+    python311Full
+  ];
+
+  # https://devenv.sh/scripts/
+  scripts.hello.exec = "echo hello from $GREET";
+
+  enterShell = ''
+    hello
+    git --version
+  '';
+
+  # https://devenv.sh/tests/
+  enterTest = ''
+    echo "Running tests"
+    git --version | grep "2.42.0"
+  '';
+
+  # See full reference at https://devenv.sh/reference/options/
+}
