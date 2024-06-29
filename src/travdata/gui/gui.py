58d62a05b74a984b91eaeccbb7c729d0528f4089
John Beisley <johnbeisleyuk@gmail.com>
2024-06-29 19:30:43 +0100

    feat: add table caching to GUI

--- src/travdata/gui/gui.py
+++ src/travdata/gui/gui.py
@@ -9,7 +9,7 @@ import sys
 from PySide6 import QtCore, QtWidgets
 
 from travdata import config
-from travdata.extraction.pdf import tabulareader
+from travdata.extraction.pdf import cachingreader, tabulareader
 from travdata.gui.extraction import cfgwin
 
 
@@ -18,12 +18,18 @@ def main() -> None:
     app = QtWidgets.QApplication(sys.argv)
     app.setQuitOnLastWindowClosed(True)
 
-    with tabulareader.TabulaClient(
-        force_subprocess=False,
-    ) as tabula_client:
+    with (
+        tabulareader.TabulaClient(
+            force_subprocess=False,
+        ) as tabula_client,
+        cachingreader.optional_table_cache(
+            delegate=tabula_client,
+            disable=False,
+        ) as table_reader,
+    ):
         window = cfgwin.ExtractionConfigWindow(
             thread_pool=QtCore.QThreadPool(),
-            table_reader=tabula_client,
+            table_reader=table_reader,
             default_config_path=config.get_default_config_path(),
         )
 
