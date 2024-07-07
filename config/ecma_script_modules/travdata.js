e407685f53b2c2ab03df4a6e2e36ae61bd284496
John Beisley <johnbeisleyuk@gmail.com>
2024-07-07 22:47:36 +0100

    chore: initial work on extracting skills packages

--- config/ecma_script_modules/travdata.js
+++ config/ecma_script_modules/travdata.js
@@ -2,6 +2,14 @@ const travdata = (function () {
   exports = {};
 
   /**
+   * Registers a function for export. 
+   * @param {Function} fn Function to export.
+   */
+  function regExport(fn) {
+    exports[fn.name] = fn;
+  }
+
+  /**
    * @typedef TableData
    * @type {string[][]}
    */
@@ -12,31 +20,33 @@ const travdata = (function () {
    * @property {TableData} data
    */
 
-  /***
+  /**
    * Concatenates the given tables.
    * @param {TableData[]} tables Tables to concatenate.
    * @returns {TableData} Result of concatenation.
    */
-  exports.concatTableData = function (tables) {
+  function concatTableData(tables) {
     const result = [];
     for (const table of tables) {
       result.splice(result.length, 0, ...table);
     }
     return result;
   };
+  regExport(concatTableData);
 
   /**
    * Returns an array of the `data` property of the given extracted tables.
    * @param {ExtractedTable[]} extTables
    * @returns {TableData[]}
    */
-  exports.tableData = function (extTables) {
+  function tableData(extTables) {
     const tables = [];
     for (const extTable of extTables) {
       tables.push(extTable.data);
     }
     return tables;
   };
+  regExport(tableData);
 
   return exports;
 })();
