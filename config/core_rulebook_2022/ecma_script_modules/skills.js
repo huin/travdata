99e14dd18cda39d35e380757f3d0477fa766f494
John Beisley <johnbeisley@proton.me>
2024-07-29 07:25:36 +0100

    refactor: skills.js to use functional state pattern

--- config/core_rulebook_2022/ecma_script_modules/skills.js
+++ config/core_rulebook_2022/ecma_script_modules/skills.js
@@ -26,13 +26,7 @@ const skills = (function () {
     }
 
     function toCsv(skills) {
-        const table = [
-            [
-                "Skill",
-                "Speciality",
-                "Description",
-            ],
-        ];
+        const table = [["Skill", "Speciality", "Description"]];
         for (const skill of skills) {
             table.push([skill.name, "", skill.desc]);
             for (const spec of skill.specs) {
@@ -48,18 +42,16 @@ const skills = (function () {
 
         // Normalise some parts that extract bullet markers into two columns,
         // such that we get a single column.
-        const lines = table.map(
-            (row) => {
-                switch (row.length) {
-                    case 0:
-                        return "";
-                    case 1:
-                        return row[0];
-                    default:
-                        return row.filter((cell) => cell.length > 0).join(" ");
-                }
+        const lines = table.map((row) => {
+            switch (row.length) {
+                case 0:
+                    return "";
+                case 1:
+                    return row[0];
+                default:
+                    return row.filter((cell) => cell.length > 0).join(" ");
             }
-        );
+        });
 
         // Accumulate specialisation.
         let specs = [];
@@ -101,65 +93,85 @@ const skills = (function () {
             specs = [];
         }
 
-        // State machine while iterating over lines.
-        const ST_FIND_SKILL = "find_skill";
-        const ST_SKILL_DESC = "skilldesc";
-        const ST_FIND_SPEC = "find_spec";
-        const ST_SPECIALITY = "spec";
-        const ST_EXAMPLE = "example";
-        let state = ST_FIND_SKILL;
+        /**
+         * @typedef StateFn
+         * @type {Function}
+         */
+
+        /**
+         * @param {string} line
+         * @returns {StateFn}
+         */
+        function stateFindSkill(line) {
+            return stateFindSkill;
+        }
+
+        /**
+         * @param {string} line
+         * @returns {StateFn}
+         */
+        function stateSkillDesc(line) {
+            if (line == SPECIALITIES_HDR) {
+                return stateFindSpec;
+            } else if (isNewExample(line)) {
+                return stateFindSkill;
+            }
+            skillDesc.push(line);
+            return stateSkillDesc;
+        }
+
+        /**
+         * @param {string} line
+         * @returns {StateFn}
+         */
+        function stateFindSpec(line) {
+            if (isNewSpeciality(line)) {
+                specLine.push(line);
+                return stateSpeciality;
+            }
+            // Still looking for first speciality.
+            return stateFindSpec;
+        }
+
+        /**
+         * @param {string} line
+         * @returns {StateFn}
+         */
+        function stateSpeciality(line) {
+            if (isNewSpeciality(line)) {
+                flushSpec();
+                specLine.push(line);
+            } else if (isNewExample(line)) {
+                flushSpec();
+                return stateExample;
+            } else {
+                specLine.push(line);
+            }
+            return stateSpeciality;
+        }
+
+        /**
+         * @param {string} line
+         * @returns {StateFn}
+         */
+        function stateExample(line) {
+            if (isNewSpeciality(line)) {
+                specLine.push(line);
+                return stateSpeciality;
+            }
+            return stateExample;
+        }
+
+        let state = stateFindSkill;
         for (const line of lines) {
             if (isSkillHeading(line)) {
                 // New skill.
                 flushSkill();
                 skillName = line;
-                state = ST_SKILL_DESC;
+                state = stateSkillDesc;
                 continue;
             }
-
-            switch (state) {
-                case ST_FIND_SKILL:
-                    break;
-
-                case ST_SKILL_DESC:
-                    if (line == SPECIALITIES_HDR) {
-                        state = ST_FIND_SPEC;
-                        continue;
-                    } else if (isNewExample(line)) {
-                        state = ST_FIND_SKILL;
-                        continue;
-                    }
-                    skillDesc.push(line);
-                    break;
-
-                case ST_FIND_SPEC:
-                    if (isNewSpeciality(line)) {
-                        specLine.push(line);
-                        state = ST_SPECIALITY;
-                    } else {
-                        // Still looking for first speciality.
-                    }
-                    break;
-
-                case ST_SPECIALITY:
-                    if (isNewSpeciality(line)) {
-                        flushSpec();
-                        specLine.push(line);
-                    } else if (isNewExample(line)) {
-                        flushSpec();
-                        state = ST_EXAMPLE;
-                    } else {
-                        specLine.push(line);
-                    }
-                    break;
-
-                case ST_EXAMPLE:
-                    if (isNewSpeciality(line)) {
-                        specLine.push(line);
-                        state = ST_SPECIALITY;
-                    }
-                    break;
-            }
+            state = state(line);
         }
         flushSkill();
 
