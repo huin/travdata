7f9003e9d3a528f6c0a521b7da3e5d0e1d79610d
John Beisley <johnbeisley@proton.me>
2024-08-03 10:10:02 +0100

    fix(rust): trivial lints

--- rust/src/cli/gui.rs
+++ rust/src/cli/gui.rs
@@ -6,7 +6,7 @@ pub fn run() -> Result<()> {
     thread::scope(|s| {
         // Run the gui in a non-main thread, as the JVM will likely want to be
         // on the main thread.
-        s.spawn(|| crate::gui::mainwin::run_gui());
+        s.spawn(crate::gui::mainwin::run_gui);
     });
 
     Ok(())
