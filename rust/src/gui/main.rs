fe39c248a2e2e3c3e2aff1f7437773f311e7f39d
John Beisley <johnbeisley@proton.me>
2025-02-24 07:34:14 +0000

    refactor: gui::main::run into smaller functions

--- rust/src/gui/main.rs
+++ rust/src/gui/main.rs
@@ -10,7 +10,7 @@ use relm4::RelmApp;
 
 use crate::{
     distpaths,
-    extraction::pdf::pdfiumthread::PdfiumServer,
+    extraction::pdf::pdfiumthread::{PdfiumClient, PdfiumServer},
     gui::{self, mainmenu, workers},
     mpscutil,
 };
@@ -26,54 +26,20 @@ pub fn run(
 
         // Run the PdfiumServer in its own dedicated thread, to serialise access to the
         // pdfium_render library.
-        let (pdfium_client_sender, pdfium_client_receiver) = mpsc::sync_channel(0);
-        s.spawn(move || {
-            let pdfium_server = match PdfiumServer::new() {
-                Ok(pdfium_server) => pdfium_server,
-                Err(err) => {
-                    mpscutil::send_or_log_warning(
-                        &pdfium_client_sender,
-                        "PdfiumServer error",
-                        Err(err),
-                    );
-                    return;
-                }
-            };
-            mpscutil::send_or_log_warning(
-                &pdfium_client_sender,
-                "PdfiumClient",
-                Ok(pdfium_server.client()),
-            );
-            pdfium_server.run();
-        });
-
-        let pdfium_client = (pdfium_client_receiver
-            .recv()
-            .with_context(|| "receiving PdfiumClient or error")?)?;
-
-        let init = gui::mainwin::Init {
-            xdg_dirs: Arc::new(xdg_dirs),
-            default_config: distpaths::config_zip(),
-            pdfium_client,
-            worker_channel: worker.worker_channel(),
-        };
+        let pdfium_client = run_pdfium_server_thread(s)?;
 
         // Run the gui in a non-main thread, as the JVM will likely want to be
         // on the main thread.
-        s.spawn(move || {
-            let program_invocation = std::env::args().next().unwrap();
-            let mut gtk_args = vec![program_invocation];
-            gtk_args.extend(gtk_options.iter().cloned());
-
-            let gtk_app = Application::builder()
-                .application_id("github.com/huin/travdata")
-                .register_session(true)
-                .build();
-            mainmenu::install_on_startup(&gtk_app);
-            install_css_on_startup(&gtk_app);
-            let app = RelmApp::from_app(gtk_app.clone()).with_args(gtk_args);
-            app.run::<gui::mainwin::MainWindow>(init);
-        });
+        run_gui_thread(
+            s,
+            gtk_options,
+            gui::mainwin::Init {
+                xdg_dirs: Arc::new(xdg_dirs),
+                default_config: distpaths::config_zip(),
+                pdfium_client,
+                worker_channel: worker.worker_channel(),
+            },
+        );
 
         // Run the extraction worker (and the JVM that it uses) in the main thread.
         worker.run();
@@ -92,5 +58,58 @@ pub fn run(
 fn install_css_on_startup(app: &Application) {
     app.connect_startup(|_| {
         relm4::set_global_css(include_str!("styles.css"));
+    });
+}
+
+fn run_pdfium_server_thread<'scope>(s: &'scope thread::Scope<'scope, '_>) -> Result<PdfiumClient> {
+    let (pdfium_client_sender, pdfium_client_receiver) = mpsc::sync_channel(0);
+
+    s.spawn(move || {
+        let pdfium_server = match PdfiumServer::new() {
+            Ok(pdfium_server) => pdfium_server,
+            Err(err) => {
+                mpscutil::send_or_log_warning(
+                    &pdfium_client_sender,
+                    "PdfiumServer error",
+                    Err(err),
+                );
+                return;
+            }
+        };
+        mpscutil::send_or_log_warning(
+            &pdfium_client_sender,
+            "PdfiumClient",
+            Ok(pdfium_server.client()),
+        );
+        pdfium_server.run();
+    });
+
+    pdfium_client_receiver
+        .recv()
+        .with_context(|| "receiving PdfiumClient or error")?
+}
+
+fn run_gui_thread<'scope, 'args>(
+    s: &'scope thread::Scope<'scope, '_>,
+    gtk_options: &'args [String],
+    init: gui::mainwin::Init,
+) where
+    'args: 'scope,
+{
+    // Run the gui in a non-main thread, as the JVM will likely want to be
+    // on the main thread.
+    s.spawn(move || {
+        let program_invocation = std::env::args().next().unwrap();
+        let mut gtk_args = vec![program_invocation];
+        gtk_args.extend(gtk_options.iter().cloned());
+
+        let gtk_app = Application::builder()
+            .application_id("github.com/huin/travdata")
+            .register_session(true)
+            .build();
+        mainmenu::install_on_startup(&gtk_app);
+        install_css_on_startup(&gtk_app);
+        let app = RelmApp::from_app(gtk_app.clone()).with_args(gtk_args);
+        app.run::<gui::mainwin::MainWindow>(init);
     });
 }
