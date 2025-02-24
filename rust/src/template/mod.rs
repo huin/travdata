02acfd81124e9c5ec49ba27fd2cabeacffa00e41
John Beisley <johnbeisley@proton.me>
2025-02-24 18:07:21 +0000

    refactor: document and rearrange ideal template structure

--- rust/src/template/mod.rs
+++ rust/src/template/mod.rs
@@ -1,35 +1,59 @@
-// XXX
 #![allow(dead_code)]
 
-use std::{collections::HashMap, path::PathBuf};
+use std::collections::{HashMap, HashSet};
 
+/// Extraction template for a single book.
 pub struct Book {
-    pub scripts: Vec<PathBuf>,
-    pub group: Group,
+    /// Source code of scripts.
+    pub scripts: Vec<String>,
+    /// Groups of templated extractions.
+    pub groups: HashMap<String, Group>,
 }
 
+/// Hierarchy of content to extract from a [Book]. The hierarchy has two effects:
+/// - Tag inheritance, identifying extracted content (for filtering and in the output).
+/// - Output directory structure for extracted data.
 pub struct Group {
+    /// Set of tags defined on the group and all its descendent [Table]s.
+    pub tags: HashSet<String>,
+    /// Child groups.
     pub groups: HashMap<String, Group>,
+    /// Child tables.
     pub tables: HashMap<String, Table>,
 }
 
+/// Template for extracting a single table.
 pub struct Table {
-    pub segments: Vec<TableSegment>,
+    /// Set of tags defined on the table (not including those from the parent [Group].
+    pub tags: HashSet<String>,
+    /// Individual raw regions of table to extract.
+    pub segments: Vec<TablePortion>,
+    /// ECMAScript function body, defining the transformation to apply to the raw extracted table
+    /// portions to define the final tabular data.
     pub transform: String,
 }
 
-pub struct TableSegment {
-    pub page: i32,
-
+/// Single raw region of tabular data to extract.
+pub struct TablePortion {
+    /// Optional key to include to identify the portion when being processed by the
+    /// [Table] `transform` function.
+    pub key: Option<String>,
     pub extraction_method: ExtractionMethod,
+    /// Zero-based index of the page to extract data from.
+    pub page: i32,
+    /// Horizontal coordinate of the left hand side of the rectangle.
     pub left: f32,
+    /// Vertical coordinate of the top of the rectangle.
     pub top: f32,
-    pub width: f32,
-    pub height: f32,
+    /// Horizontal coordinate of the right hand side of the rectangle.
+    pub right: f32,
+    /// Vertical coordinate of the bottom of the rectangle.
+    pub bottom: f32,
 }
 
+/// Extraction algorithm for Tabula to use.
 pub enum ExtractionMethod {
-    Stream,
     Guess,
     Lattice,
+    Stream,
 }
