d5ef1585f9829b13243f8723d32f602a5e76388d
John Beisley <johnbeisley@proton.me>
2025-04-10 19:29:52 +0100

    feat: experiment with merging edits

--- rust/src/gui/tmplmodel/tests.rs
+++ rust/src/gui/tmplmodel/tests.rs
@@ -5,6 +5,7 @@ use super::*;
 
 const INITIAL_NAME: &str = "initial name";
 const NEW_NAME: &str = "new name";
+const SECOND_NEW_NAME: &str = "second new name";
 
 #[googletest::test]
 fn test_undo_redo() -> Result<()> {
@@ -45,6 +46,25 @@ fn test_edit_table_name() -> Result<()> {
     table.edit_name(NEW_NAME.to_string())?;
     assert_that!(table.get_name()?, eq(NEW_NAME));
 
+    doc.undo()?;
+    assert_that!(table.get_name()?, eq(INITIAL_NAME));
+
+    Ok(())
+}
+
+#[googletest::test]
+fn test_edit_table_name_merges_consecutive_edits() -> Result<()> {
+    let doc = DocumentRc::new();
+
+    let table = doc.new_table(TableData {
+        name: INITIAL_NAME.to_string(),
+        ..Default::default()
+    });
+    table.edit_name(NEW_NAME.to_string())?;
+    table.edit_name(SECOND_NEW_NAME.to_string())?;
+    assert_that!(table.get_name()?, eq(SECOND_NEW_NAME));
+
+    // Should skip over NEW_NAME because the edits were merged.
     doc.undo()?;
     assert_that!(table.get_name()?, eq(INITIAL_NAME));
 
