734da1531209371d6ddf8caf9795e531e1b3ad89
John Beisley <johnbeisley@proton.me>
2025-09-11 06:57:36 +0100

    feat: rewrite v8wrapper to use thread local storage

--- rust/v8wrapper/src/lib.rs
+++ rust/v8wrapper/src/lib.rs
@@ -4,18 +4,14 @@
 
 #[cfg(test)]
 mod test;
-#[cfg(any(feature = "testing", test))]
-pub mod testisolate;
 
-use std::{any::Any, collections::HashMap, sync::mpsc};
-
-use anyhow::{Context, Error, Result, anyhow};
-
-use utils::mpscutil;
+use anyhow::{Context, Error, Result, anyhow, bail};
 
 static INIT_V8: std::sync::OnceLock<()> = std::sync::OnceLock::new();
 
-fn init_v8() {
+/// Initialises [v8]. Must be called before any other functions in [v8] or this crate. Can safely
+/// be called multiple times. Must be called from the main thread.
+pub fn init_v8() {
     INIT_V8.get_or_init(|| {
         let platform = v8::new_default_platform(0, false).make_shared();
         v8::V8::initialize_platform(platform);
@@ -23,17 +19,18 @@ fn init_v8() {
     });
 }
 
+/// Initialises [v8]. Must be called before any other functions in [v8] or this crate. Can safely
+/// be called multiple times. May be called from any thread, but provides fewer security
+/// protections, and so is appropriate to call from tests.
+pub fn init_v8_for_testing() {
+    INIT_V8.get_or_init(|| {
+        let platform = v8::new_unprotected_default_platform(0, false).make_shared();
+        v8::V8::initialize_platform(platform);
+        v8::V8::initialize();
+    });
+}
+
 pub type TryScope<'s, 'p> = v8::TryCatch<'s, v8::HandleScope<'p>>;
-
-type RunResult = Result<Box<dyn Any + Send + 'static>>;
-
-trait RunFn: FnOnce(&mut TryScope<'_, '_>) -> RunResult + Send {}
-impl<F> RunFn for F where F: FnOnce(&mut TryScope<'_, '_>) -> RunResult + Send {}
-
-type BoxedRunFn = Box<dyn RunFn>;
-
-#[derive(Clone, Debug, Hash, PartialEq, Eq)]
-struct ContextKey(u32);
 
 /// Origin of some code.
 #[derive(Debug)]
@@ -77,244 +74,108 @@ impl Default for ESScriptOrigin {
     }
 }
 
-/// Single-owner of a thread that manages requests to be made on a [v8::Isolate].
-///
-/// Only one of these should be created within a process, due to a bug that causes a segfault if
-/// the [v8] crate creates a second [v8::Isolate].
-#[derive(Debug)]
-pub struct IsolateThreadHandle {
-    // The declaration order of request_send and thread_join is such that they will be dropped in
-    // an order that shuts down correctly. Dropping `request_send` implicitly requests that the
-    // thread stops waiting for more requests.
-    request_send: mpsc::SyncSender<Request>,
-    // `thread_join` is kept so that `Drop` blocks until the thread shuts down.
-    #[allow(dead_code)]
-    thread_join: std::thread::JoinHandle<()>,
+thread_local! {
+    static ISOLATE: std::cell::RefCell<Option<TlsIsolateGuard>> = const {
+        std::cell::RefCell::new(None)
+    };
 }
 
-impl IsolateThreadHandle {
-    pub fn new() -> Self {
-        let (request_send, request_recv) = mpsc::sync_channel(0);
+/// Upon creation creates a [v8::OwnedIsolate] for the current thread, and destroys it upon [Drop].
+pub struct TlsIsolate(
+    // Uses the `PhantomData *const` to implicitly mark as neither [Send] nor [Sync] until
+    // https://github.com/rust-lang/rust/issues/68318 is resolved.
+    std::marker::PhantomData<*const ()>,
+);
 
-        let thread_join = std::thread::spawn(move || run_isolate_thread(request_recv));
-
-        Self {
-            request_send,
-            thread_join,
-        }
-    }
-
-    pub fn create_client(&self) -> IsolateThreadClient {
-        IsolateThreadClient {
-            request_send: self.request_send.clone(),
-        }
-    }
-
-    /// Creates a new [v8::Context] and returns a client to act on it.
-    pub fn new_context(&self) -> Result<ContextClient> {
-        ContextClient::new(self.request_send.clone())
+impl TlsIsolate {
+    /// Creates a [TlsIsolate] for the current thread, which enables calls to [try_with_isolate]
+    /// until it is dropped.
+    ///
+    /// This explicitly manages the lifetime of the thread local [v8::OwnedIsolate].
+    pub fn for_current_thread() -> Result<Self> {
+        ISOLATE.try_with(|isolate_refcell| {
+            if isolate_refcell.borrow().is_some() {
+                bail!("TlsIsolate already exists for this thread");
+            }
+            isolate_refcell.replace(Some(TlsIsolateGuard::new()));
+            Ok(Self(std::marker::PhantomData))
+        })?
     }
 }
 
-#[derive(Debug, Clone)]
-pub struct IsolateThreadClient {
-    request_send: mpsc::SyncSender<Request>,
-}
-
-impl IsolateThreadClient {
-    /// Creates a new [v8::Context] and returns a client to act on it.
-    pub fn new_context(&self) -> Result<ContextClient> {
-        ContextClient::new(self.request_send.clone())
-    }
-}
-
-/// Owns a [v8::Context] within an [IsolateThreadClient], cleans up the [v8::Context] when dropped.
-pub struct ContextClient {
-    ctx_key: ContextKey,
-    request_send: mpsc::SyncSender<Request>,
-}
-
-impl ContextClient {
-    fn new(request_send: mpsc::SyncSender<Request>) -> Result<Self> {
-        let (result_send, result_recv) = mpsc::sync_channel(0);
-        request_send
-            .send(Request::NewContext(result_send))
-            .map_err(|err| anyhow!("{}", err))
-            .context("sending request to IsolateThread")?;
-        let ctx_key = result_recv
-            .recv()
-            .context("receiving result from IsolateThread")?
-            .context("failed to allocate Context")?;
-
-        Ok(ContextClient {
-            ctx_key,
-            request_send,
-        })
-    }
-
-    /// Runs the closure `f` against the [v8::Context] on the isolate thread.
-    pub fn run<F, T>(&self, f: F) -> Result<T>
-    where
-        F: FnOnce(&mut TryScope) -> Result<T> + Send + 'static,
-        T: Any + Send + 'static,
-    {
-        let (result_send, result_recv) = mpsc::sync_channel(0);
-
-        self.request_send
-            .send(Request::Run {
-                ctx_key: self.ctx_key.clone(),
-                func: Box::new(|context: &mut TryScope| -> RunResult {
-                    let value = f(context)?;
-                    Ok(Box::new(value))
-                }),
-                result_send,
-            })
-            .map_err(|err| anyhow!("{}", err))
-            .context("sending request to IsolateThread")?;
-
-        result_recv
-            .recv()
-            .context("receiving result from IsolateThread")?
-            .and_then(|value| {
-                value.downcast::<T>().map_err(|err| {
-                    anyhow!(
-                        "received wrong type in result from IsolateThread (got value {:?})",
-                        err,
-                    )
-                })
-            })
-            .map(|value| *value)
-    }
-}
-
-impl Drop for ContextClient {
+impl Drop for TlsIsolate {
     fn drop(&mut self) {
-        let result = self
-            .request_send
-            .send(Request::DropContext(self.ctx_key.clone()))
-            .map_err(|err| anyhow!("{}", err))
-            .context("sending request to IsolateThread");
+        let result = ISOLATE.try_with(|isolate_refcell| {
+            let _ = isolate_refcell.borrow_mut().take();
+        });
         if let Err(err) = result {
-            log::error!("failed to drop ContextClient: {:?}", err);
+            log::warn!("Could not drop TlsIsolate: {err}.");
         }
     }
 }
 
-/// Entry point that creates an internal [IsolateThread] and dispatches requests to it.
-///
-/// This method blocks until the given `request_recv` is closed.
-fn run_isolate_thread(request_recv: mpsc::Receiver<Request>) {
-    init_v8();
-
-    let mut isolate = v8::Isolate::new(v8::CreateParams::default());
-    let mut scope = v8::HandleScope::new(&mut isolate);
-
-    let mut processor = IsolateThread::new(&mut scope);
-    processor.perform_requests(request_recv);
-}
-
-struct IsolateThread<'s, 'i> {
-    isolate_scope: &'i mut v8::HandleScope<'s, ()>,
-    contexts: HashMap<ContextKey, v8::Local<'s, v8::Context>>,
-    next_ctx_key: ContextKey,
-    free_ctx_keys: Vec<ContextKey>,
-}
-
-impl<'s, 'i> IsolateThread<'s, 'i>
+/// Runs the given lambda with the thread's local v8 isolate.
+pub fn try_with_isolate<F, R>(f: F) -> Result<R, TlsIsolateError>
 where
-    's: 'i,
+    F: FnOnce(&mut TlsIsolateGuard) -> R,
 {
-    fn new(isolate_scope: &'i mut v8::HandleScope<'s, ()>) -> Self {
+    ISOLATE
+        .try_with(|isolate| -> Result<R, TlsIsolateError> {
+            let mut isolate_opt_ref = isolate.borrow_mut();
+            let isolate_ref = isolate_opt_ref.as_mut().ok_or(TlsIsolateError::NotExist)?;
+            Ok(f(isolate_ref))
+        })
+        .map_err(TlsIsolateError::AccessError)
+        .and_then(|r| r)
+}
+
+pub struct TlsIsolateGuard {
+    isolate: v8::OwnedIsolate,
+}
+
+impl TlsIsolateGuard {
+    fn new() -> Self {
         Self {
-            isolate_scope,
-            contexts: HashMap::new(),
-            next_ctx_key: ContextKey(0),
-            free_ctx_keys: Vec::new(),
+            isolate: v8::Isolate::new(v8::CreateParams::default()),
         }
     }
 
-    fn perform_requests(&mut self, request_recv: mpsc::Receiver<Request>) {
-        while let Ok(request) = request_recv.recv() {
-            match request {
-                Request::NewContext(result_send) => {
-                    let result = self.handle_new_context();
-                    mpscutil::send_or_log_warning(
-                        &result_send,
-                        "result of call to IsolateThread::handle_new_context",
-                        result,
-                    );
-                }
-                Request::DropContext(ctx_key) => {
-                    self.handle_drop_context(ctx_key);
-                }
-                Request::Run {
-                    ctx_key,
-                    func,
-                    result_send,
-                } => {
-                    let result = self.handle_run(ctx_key, func);
-                    mpscutil::send_or_log_warning(
-                        &result_send,
-                        "result of call to IsolateThread::handle_run",
-                        result,
-                    );
-                }
+    pub fn isolate(&mut self) -> &mut v8::OwnedIsolate {
+        &mut self.isolate
+    }
+
+    pub fn scope<'s>(&'s mut self) -> v8::HandleScope<'s, ()> {
+        v8::HandleScope::new(&mut self.isolate)
+    }
+
+    pub fn new_ctx(&mut self) -> v8::Global<v8::Context> {
+        let mut scope = self.scope();
+        let ctx = v8::Context::new(&mut scope, v8::ContextOptions::default());
+        v8::Global::new(&mut scope, ctx)
+    }
+}
+
+/// Describes the cause of an error with [try_with_isolate].
+#[derive(Debug)]
+pub enum TlsIsolateError {
+    /// Error accessing the thread local.
+    AccessError(std::thread::AccessError),
+    /// [TlsIsolate] does not exist for the current thread.
+    NotExist,
+}
+
+impl std::fmt::Display for TlsIsolateError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            TlsIsolateError::AccessError(_) => write!(f, "AccessError"),
+            TlsIsolateError::NotExist => {
+                write!(f, "TlsIsolate does not exist on the current thread")
             }
         }
     }
-
-    fn handle_new_context(&mut self) -> Result<ContextKey> {
-        let ctx_key = self
-            .new_ctx_key()
-            .ok_or_else(|| anyhow!("run out of context keys"))?;
-
-        let context = v8::Context::new(self.isolate_scope, v8::ContextOptions::default());
-        self.contexts.insert(ctx_key.clone(), context);
-        Ok(ctx_key)
-    }
-
-    fn handle_drop_context(&mut self, ctx_key: ContextKey) {
-        if self.contexts.remove(&ctx_key).is_some() {
-            self.recycle_ctx_key(ctx_key);
-        }
-    }
-
-    fn handle_run(&mut self, ctx_key: ContextKey, func: BoxedRunFn) -> Result<Box<dyn Any + Send>> {
-        let context = self
-            .contexts
-            .get(&ctx_key)
-            .ok_or_else(|| anyhow!("invalid context key {:?}", ctx_key))?;
-        let context_scope = &mut v8::ContextScope::new(self.isolate_scope, *context);
-        let handle_scope = &mut v8::HandleScope::new(context_scope);
-        let try_catch = &mut v8::TryCatch::new(handle_scope);
-
-        func(try_catch)
-    }
-
-    fn new_ctx_key(&mut self) -> Option<ContextKey> {
-        if let Some(ctx_key) = self.free_ctx_keys.pop() {
-            Some(ctx_key)
-        } else {
-            let ctx_key_value = self.next_ctx_key.clone();
-            self.next_ctx_key = self.next_ctx_key.0.checked_add(1).map(ContextKey)?;
-            Some(ctx_key_value)
-        }
-    }
-
-    fn recycle_ctx_key(&mut self, ctx_key: ContextKey) {
-        self.free_ctx_keys.push(ctx_key);
-    }
 }
 
-enum Request {
-    NewContext(mpsc::SyncSender<Result<ContextKey>>),
-    DropContext(ContextKey),
-    Run {
-        ctx_key: ContextKey,
-        func: BoxedRunFn,
-        result_send: mpsc::SyncSender<RunResult>,
-    },
-}
+impl std::error::Error for TlsIsolateError {}
 
 /// Always returns an `Err`, but will use information from the given [v8::TryCatch] if a message is
 /// present.
--- rust/v8wrapper/src/test.rs
+++ rust/v8wrapper/src/test.rs
@@ -1,124 +1,222 @@
+use anyhow::Result;
 use googletest::prelude::*;
 use testutils::WrapError;
 
-use super::{testisolate::IsolateThreadHandleForTest, *};
+use super::*;
 
 #[gtest]
-fn test_thread_isolate_create_and_call_function(
-    handle: &&IsolateThreadHandleForTest,
-) -> googletest::Result<()> {
-    let ctx_client = handle.new_context().wrap_error()?;
+fn test_thread_isolate_create_and_call_function() -> googletest::Result<()> {
+    init_v8_for_testing();
 
-    let result: f64 = ctx_client
-        .run(|try_catch| {
-            let func_v8 = new_v8_function(
-                try_catch,
-                &["arg1"],
-                &ESScriptOrigin::default(),
-                r#"return arg1 + 2"#,
-            )?;
+    let tls_isolate = TlsIsolate::for_current_thread().wrap_error()?;
 
-            let global = try_catch.get_current_context().global(try_catch);
-            let arg1_v8 = new_v8_number(try_catch, 3.0);
-            let result_v8 = func_v8
-                .call(try_catch, global.into(), &[arg1_v8.into()])
-                .ok_or_else(|| try_catch_to_result(try_catch))
-                .context("calling function")?;
+    let result: f64 = try_with_isolate(|tls_isolate| -> Result<f64> {
+        let mut scope = tls_isolate.scope();
+        let ctx = v8::Context::new(&mut scope, v8::ContextOptions::default());
 
-            let result: f64 = result_v8
-                .number_value(try_catch)
-                .ok_or_else(|| anyhow!("expected number, got {}", result_v8.type_repr()))
-                .context("casting result to number")?;
+        let mut ctx_scope = v8::ContextScope::new(&mut scope, ctx);
+        let try_catch = &mut v8::TryCatch::new(&mut ctx_scope);
 
-            Ok(result)
-        })
-        .context("calling client.run")
-        .wrap_error()?;
+        let func_v8 = new_v8_function(
+            try_catch,
+            &["arg1"],
+            &ESScriptOrigin::default(),
+            r#"return arg1 + 2"#,
+        )?;
+
+        let global = ctx.global(try_catch);
+        let arg1_v8 = new_v8_number(try_catch, 3.0);
+        let result_v8 = func_v8
+            .call(try_catch, global.into(), &[arg1_v8.into()])
+            .ok_or_else(|| try_catch_to_result(try_catch))
+            .context("calling function")?;
+
+        let result: f64 = result_v8
+            .number_value(try_catch)
+            .ok_or_else(|| anyhow!("expected number, got {}", result_v8.type_repr()))
+            .context("casting result to number")?;
+
+        Ok(result)
+    })?
+    .wrap_error()?;
 
     assert_that!(result, approx_eq(5.0));
+
+    drop(tls_isolate);
 
     Ok(())
 }
 
 #[gtest]
-fn test_thread_isolate_create_store_and_later_use_function(
-    handle: &&IsolateThreadHandleForTest,
-) -> googletest::Result<()> {
+fn test_thread_isolate_create_store_and_later_use_function() -> googletest::Result<()> {
+    init_v8_for_testing();
+
+    let tls_isolate = TlsIsolate::for_current_thread().wrap_error()?;
+
     // Given two contexts.
-    let ctx_client_1 = handle.new_context().wrap_error()?;
-    let ctx_client_2 = handle.new_context().wrap_error()?;
+    let (ctx_1, ctx_2) =
+        try_with_isolate(|tls_isolate| (tls_isolate.new_ctx(), tls_isolate.new_ctx()))?;
 
     const FUNC_NAME: &str = "my_func";
 
     // Given a function is created on the first context's global.
-    ctx_client_1
-        .run(|try_catch| {
-            let func_v8 = new_v8_function(
-                try_catch,
-                &["arg1"],
-                &ESScriptOrigin::default(),
-                r#"return arg1 + 2"#,
-            )?;
+    try_with_isolate(|tls_isolate| {
+        let mut scope = tls_isolate.scope();
+        let ctx = v8::Local::new(&mut scope, &ctx_1);
+        let mut ctx_scope = v8::ContextScope::new(&mut scope, ctx);
+        let try_catch = &mut v8::TryCatch::new(&mut ctx_scope);
 
-            let global = try_catch.get_current_context().global(try_catch);
-            let func_name_v8 =
-                new_v8_string(try_catch, FUNC_NAME).context("creating function name string")?;
+        let func_v8 = new_v8_function(
+            try_catch,
+            &["arg1"],
+            &ESScriptOrigin::default(),
+            r#"return arg1 + 2"#,
+        )?;
 
-            global
-                .set(try_catch, func_name_v8.into(), func_v8.into())
-                .context("setting function on global object")?;
+        let global = try_catch.get_current_context().global(try_catch);
+        let func_name_v8 =
+            new_v8_string(try_catch, FUNC_NAME).context("creating function name string")?;
 
-            Ok(())
-        })
-        .context("calling client.run")
-        .wrap_error()?;
+        global
+            .set(try_catch, func_name_v8.into(), func_v8.into())
+            .context("setting function on global object")?;
+
+        Ok(())
+    })?
+    .wrap_error()?;
 
     // Then calling the function in the first context should work and return the expected answer.
-    let result = ctx_client_1
-        .run(|try_catch| {
-            let global = try_catch.get_current_context().global(try_catch);
-            let func_name_v8 =
-                new_v8_string(try_catch, FUNC_NAME).context("creating function name string")?;
+    let result = try_with_isolate(|tls_isolate| -> Result<f64> {
+        let mut scope = tls_isolate.scope();
+        let ctx = v8::Local::new(&mut scope, &ctx_1);
+        let mut ctx_scope = v8::ContextScope::new(&mut scope, ctx);
+        let try_catch = &mut v8::TryCatch::new(&mut ctx_scope);
 
-            let func_v8 = global
-                .get(try_catch, func_name_v8.into())
-                .context("getting function from global object")?
-                .try_cast::<v8::Function>()
-                .context("casting Value to Function")?;
+        let global = try_catch.get_current_context().global(try_catch);
+        let func_name_v8 =
+            new_v8_string(try_catch, FUNC_NAME).context("creating function name string")?;
 
-            let arg1_v8 = new_v8_number(try_catch, 3.0);
-            let result_v8 = func_v8
-                .call(try_catch, global.into(), &[arg1_v8.into()])
-                .ok_or_else(|| try_catch_to_result(try_catch))
-                .context("calling function")?;
+        let func_v8 = global
+            .get(try_catch, func_name_v8.into())
+            .context("getting function from global object")?
+            .try_cast::<v8::Function>()
+            .context("casting Value to Function")?;
 
-            let result: f64 = result_v8
-                .number_value(try_catch)
-                .ok_or_else(|| anyhow!("expected number, got {}", result_v8.type_repr()))
-                .context("casting result to number")?;
+        let arg1_v8 = new_v8_number(try_catch, 3.0);
+        let result_v8 = func_v8
+            .call(try_catch, global.into(), &[arg1_v8.into()])
+            .ok_or_else(|| try_catch_to_result(try_catch))
+            .context("calling function")?;
 
-            Ok(result)
-        })
-        .context("calling client.run")
-        .wrap_error()?;
+        let result: f64 = result_v8
+            .number_value(try_catch)
+            .ok_or_else(|| anyhow!("expected number, got {}", result_v8.type_repr()))
+            .context("casting result to number")?;
+
+        Ok(result)
+    })?
+    .wrap_error()?;
     assert_that!(result, approx_eq(5.0));
 
     // Then the function should not be present in the second context.
-    let func_existed_on_other_context = ctx_client_2
-        .run(|try_catch| {
-            let global = try_catch.get_current_context().global(try_catch);
-            let func_name_v8 =
-                new_v8_string(try_catch, FUNC_NAME).context("creating function name string")?;
+    let func_existed_on_other_context = try_with_isolate(|tls_isolate| -> Result<bool> {
+        let mut scope = tls_isolate.scope();
+        let ctx = v8::Local::new(&mut scope, &ctx_2);
+        let mut ctx_scope = v8::ContextScope::new(&mut scope, ctx);
+        let try_catch = &mut v8::TryCatch::new(&mut ctx_scope);
 
-            let func_v8 = global
-                .get(try_catch, func_name_v8.into())
-                .context("getting function from global object")?;
+        let global = try_catch.get_current_context().global(try_catch);
+        let func_name_v8 =
+            new_v8_string(try_catch, FUNC_NAME).context("creating function name string")?;
 
-            Ok(!func_v8.is_undefined())
-        })
-        .context("calling client.run")
-        .wrap_error()?;
+        let func_v8 = global
+            .get(try_catch, func_name_v8.into())
+            .context("getting function from global object")?;
+
+        Ok(!func_v8.is_undefined())
+    })?
+    .wrap_error()?;
     assert_that!(func_existed_on_other_context, is_false());
+
+    drop(tls_isolate);
+
+    Ok(())
+}
+
+#[gtest]
+fn test_set_and_get_values_in_separate_contexts() -> googletest::Result<()> {
+    fn set_value(
+        tls_isolate: &mut TlsIsolateGuard,
+        ctx_global: &v8::Global<v8::Context>,
+        value: &str,
+    ) -> Result<()> {
+        let mut scope = tls_isolate.scope();
+        let ctx = v8::Local::new(&mut scope, ctx_global);
+
+        let mut ctx_scope = v8::ContextScope::new(&mut scope, ctx);
+        let try_catch = &mut v8::TryCatch::new(&mut ctx_scope);
+
+        let key = new_v8_string(try_catch, "foo")?;
+        let value = new_v8_string(try_catch, value)?;
+
+        let global_obj = ctx.global(try_catch);
+        global_obj
+            .set(try_catch, key.cast(), value.cast())
+            .ok_or_else(|| try_catch_to_result(try_catch))?;
+
+        Ok(())
+    }
+
+    fn get_value(
+        tls_isolate: &mut TlsIsolateGuard,
+        ctx_global: &v8::Global<v8::Context>,
+    ) -> Result<String> {
+        let mut scope = tls_isolate.scope();
+
+        let ctx = v8::Local::new(&mut scope, ctx_global);
+        let mut ctx_scope = v8::ContextScope::new(&mut scope, ctx);
+        let try_catch = &mut v8::TryCatch::new(&mut ctx_scope);
+
+        let global_obj = ctx.global(try_catch);
+
+        let key = new_v8_string(try_catch, "foo")?;
+        Ok(global_obj
+            .get(try_catch, key.cast())
+            .ok_or_else(|| try_catch_to_result(try_catch))?
+            .try_cast::<v8::String>()?
+            .to_rust_string_lossy(try_catch))
+    }
+
+    const SET_VALUE_1: &str = "value in context 1";
+    const SET_VALUE_2: &str = "value in context 2";
+
+    init_v8_for_testing();
+
+    let tls_isolate = TlsIsolate::for_current_thread().wrap_error()?;
+
+    let (ctx_1_global, ctx_2_global) =
+        try_with_isolate(|tls_isolate| (tls_isolate.new_ctx(), tls_isolate.new_ctx()))?;
+
+    try_with_isolate(|tls_isolate| -> Result<()> {
+        set_value(tls_isolate, &ctx_1_global, SET_VALUE_1)?;
+        set_value(tls_isolate, &ctx_2_global, SET_VALUE_2)?;
+
+        Ok(())
+    })?
+    .wrap_error()?;
+
+    let (value_1, value_2) = try_with_isolate(|tls_isolate| -> Result<(String, String)> {
+        let value_1 = get_value(tls_isolate, &ctx_1_global)?;
+        let value_2 = get_value(tls_isolate, &ctx_2_global)?;
+
+        Ok((value_1, value_2))
+    })?
+    .wrap_error()?;
+
+    assert_that!(value_1, eq(SET_VALUE_1));
+    assert_that!(value_2, eq(SET_VALUE_2));
+
+    drop(tls_isolate);
 
     Ok(())
 }
