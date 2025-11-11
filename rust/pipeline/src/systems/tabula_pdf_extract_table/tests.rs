0be30d07a6ea14d86bf2c16bc83626dc0aa5d1c4
John Beisley <johnbeisley@proton.me>
2025-11-11 06:53:09 +0000

    refactor(pipeline,test): PDF extract tests test process_multiple

--- rust/pipeline/src/systems/tabula_pdf_extract_table/tests.rs
+++ rust/pipeline/src/systems/tabula_pdf_extract_table/tests.rs
@@ -5,8 +5,8 @@ use test_casing::{cases, test_casing};
 use testutils::DefaultForTest;
 
 use crate::{
-    intermediates, spec_types::pdf, tabula_wrapper::singlethreaded::SingleThreadedTabulaExtractor,
-    testutil::node_id,
+    NodeId, intermediates, spec_types::pdf,
+    tabula_wrapper::singlethreaded::SingleThreadedTabulaExtractor, testutil::node_id,
 };
 
 use super::*;
@@ -20,72 +20,111 @@ lazy_static! {
 
 #[derive(Debug)]
 struct NodeTestCase {
-    node_spec: crate::specs::PdfExtractTable,
-    expected_data: &'static [&'static [&'static str]],
     skip: bool,
+    nodes: Vec<Node>,
+    expected_output: &'static [NodeOutput],
+}
+
+#[derive(Debug)]
+struct NodeOutput {
+    node_id: &'static str,
+    data: &'static [&'static [&'static str]],
 }
 
 const NODE_CASES: test_casing::TestCases<NodeTestCase> = cases! {
     [
         NodeTestCase {
-            node_spec: crate::specs::PdfExtractTable {
-                pdf: node_id("pdf-file"),
-                page: 1,
-                method: TabulaExtractionMethod::Lattice,
-                rect: pdf::TabulaPdfRect {
-                    left: 52.0.into(),
-                    top: 88.0.into(),
-                    right: (52.0 + 489.0).into(),
-                    bottom: (88.0 + 67.0).into(),
-                },
-            },
-            expected_data: &[
-                &["Heading 1", "Heading 2", "Heading 3"],
-                &["r1c1", "r1c2", "r1c3"],
-                &["r2c1", "r2c2", "r2c3"],
-            ],
             skip: false,
+            nodes: vec![
+                Node {
+                    id: node_id("node-one"),
+                    spec: crate::specs::PdfExtractTable {
+                        pdf: node_id("pdf-file"),
+                        page: 1,
+                        method: TabulaExtractionMethod::Lattice,
+                        rect: pdf::TabulaPdfRect {
+                            left: 52.0.into(),
+                            top: 88.0.into(),
+                            right: (52.0 + 489.0).into(),
+                            bottom: (88.0 + 67.0).into(),
+                        },
+                    }.into(),
+                    ..DefaultForTest::default_for_test()
+                },
+            ],
+            expected_output: &[
+                NodeOutput {
+                    node_id: "node-one",
+                    data: &[
+                        &["Heading 1", "Heading 2", "Heading 3"],
+                        &["r1c1", "r1c2", "r1c3"],
+                        &["r2c1", "r2c2", "r2c3"],
+                    ],
+                },
+            ],
         },
         NodeTestCase {
-            node_spec: crate::specs::PdfExtractTable {
-                pdf: node_id("pdf-file"),
-                page: 1,
-                method: TabulaExtractionMethod::Stream,
-                rect: pdf::TabulaPdfRect {
-                    left: 52.0.into(),
-                    top: 186.0.into(),
-                    right: (52.0 + 489.0).into(),
-                    bottom: (186.0 + 67.0).into(),
-                },
-            },
-            expected_data: &[
-                &["Heading 1", "Heading 2", "Heading 3"],
-                &["r1c1", "r1c2", "r1c3"],
-                &["r2c1", "r2c2", "r2c3"],
-            ],
             skip: false,
+            nodes: vec![
+                Node {
+                    id: node_id("node-one"),
+                    spec: crate::specs::PdfExtractTable {
+                        pdf: node_id("pdf-file"),
+                        page: 1,
+                        method: TabulaExtractionMethod::Stream,
+                        rect: pdf::TabulaPdfRect {
+                            left: 52.0.into(),
+                            top: 186.0.into(),
+                            right: (52.0 + 489.0).into(),
+                            bottom: (186.0 + 67.0).into(),
+                        },
+                    }.into(),
+                    ..DefaultForTest::default_for_test()
+                },
+            ],
+            expected_output: &[
+                NodeOutput {
+                    node_id: "node-one",
+                    data: &[
+                        &["Heading 1", "Heading 2", "Heading 3"],
+                        &["r1c1", "r1c2", "r1c3"],
+                        &["r2c1", "r2c2", "r2c3"],
+                    ],
+                },
+            ],
         },
         NodeTestCase {
-            node_spec: crate::specs::PdfExtractTable {
-                pdf: node_id("pdf-file"),
-                page: 1,
-                method: TabulaExtractionMethod::Lattice,
-                rect: pdf::TabulaPdfRect {
-                    left: 52.0.into(),
-                    top: 275.0.into(),
-                    right: (52.0 + 489.0).into(),
-                    bottom: (275.0 + 149.0).into(),
-                },
-            },
-            expected_data: &[
-                &["Heading 1", "Heading 2", "Heading 3"],
-                &["r1c1", "r1c2", "r1c3"],
-                &["r2c1", "r2c2", "r2c3"],
-                &["r3c1", "r3c2", "r3c3"],
-                &["r4c1", "r4c2", "r4c3"],
-            ],
             // TODO: fix this case.
             skip: true,
+            nodes: vec![
+                Node {
+                    id: node_id("node-one"),
+                    spec: crate::specs::PdfExtractTable {
+                        pdf: node_id("pdf-file"),
+                        page: 1,
+                        method: TabulaExtractionMethod::Lattice,
+                        rect: pdf::TabulaPdfRect {
+                            left: 52.0.into(),
+                            top: 275.0.into(),
+                            right: (52.0 + 489.0).into(),
+                            bottom: (275.0 + 149.0).into(),
+                        },
+                    }.into(),
+                    ..DefaultForTest::default_for_test()
+                },
+            ],
+            expected_output: &[
+                NodeOutput {
+                    node_id: "node-one",
+                    data: &[
+                        &["Heading 1", "Heading 2", "Heading 3"],
+                        &["r1c1", "r1c2", "r1c3"],
+                        &["r2c1", "r2c2", "r2c3"],
+                        &["r3c1", "r3c2", "r3c3"],
+                        &["r4c1", "r4c2", "r4c3"],
+                    ],
+                },
+            ],
         },
     ]
 };
@@ -102,40 +141,52 @@ fn test_process_uses_intermediate_values(test_case: NodeTestCase) -> Result<()> 
         return Ok(());
     }
 
-    let vm = &VM.as_ref().unwrap();
+    let vm = VM.as_ref().unwrap();
     let env = vm.attach()?;
     let extractor = SingleThreadedTabulaExtractor::new(env);
 
     let system = TabulaPdfExtractTableSystem::new(&extractor);
 
-    let node = crate::Node {
-        spec: test_case.node_spec.into(),
-        ..DefaultForTest::default_for_test()
-    };
+    let node_refs: Vec<&Node> = test_case.nodes.iter().collect();
 
-    let expected: intermediates::IntermediateValue =
-        intermediates::JsonData(serde_json::Value::Array(
-            test_case
-                .expected_data
-                .iter()
-                .map(|&static_row| {
-                    static_row
-                        .iter()
-                        .map(|&static_field| serde_json::Value::String(static_field.into()))
-                        .collect()
-                })
-                .collect(),
-        ))
-        .into();
+    let expected_interms: HashMap<NodeId, intermediates::IntermediateValue> = test_case
+        .expected_output
+        .iter()
+        .map(|node_output| {
+            (
+                node_id(node_output.node_id),
+                intermediates::JsonData(table_slice_to_to_json_value(node_output.data)).into(),
+            )
+        })
+        .collect();
 
     let mut interms = intermediates::IntermediateSet::new();
     interms.set(
         node_id("pdf-file"),
         intermediates::InputFile("./test_data/tables.pdf".into()).into(),
     );
-    let got = system.process(&node, &Default::default(), &interms);
+    let got_results = system.process_multiple(&node_refs, &Default::default(), &interms);
 
-    expect_that!(&got, ok(eq(&expected)));
+    let got_interims: HashMap<NodeId, intermediates::IntermediateValue> = got_results
+        .into_iter()
+        .map(|node_result| Ok((node_result.id, node_result.value?)))
+        .collect::<anyhow::Result<HashMap<_, _>>>()?;
+
+    expect_that!(got_interims, eq(&expected_interms));
 
     Ok(())
+}
+
+fn table_slice_to_to_json_value(table_slice: &[&[&str]]) -> serde_json::Value {
+    serde_json::Value::Array(
+        table_slice
+            .iter()
+            .map(|&static_row| {
+                static_row
+                    .iter()
+                    .map(|&static_field| serde_json::Value::String(static_field.into()))
+                    .collect()
+            })
+            .collect(),
+    )
 }
